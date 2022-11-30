909418d7237269ded7463f8a39dd93b25eb7be18
floresclaudiaa <flores.claudia.a@gmail.com>
2022-11-30 01:03:39 +0000

    signature password file generation

--- gpg_sign_files.py
+++ gpg_sign_files.py
@@ -4,15 +4,36 @@ from timeit import timeit
 
 SIGS_DIR = Path.cwd() / "sigs"
 DATA_DIR = Path.cwd() / "data"
+KEYS_DIR = Path.cwd() / "keys"
 
 data = sorted(listdir(DATA_DIR))
+all_keys_path = KEYS_DIR / "all_keys"
 
-#### Find optimal way to retrieve keys from GPG ring
+#### Ensure only keys needed are available in key ring; TODO: trustdb
 
-keys = []
+def get_key_type(fname: str) -> str:
+    return fname.split("_")[0]
 
-def sign(fname: str, key: str):
-    system(f"gpg --clear-sign {fname} -o ./sigs/{fname}.gpg")
+def get_key_len(fname: str) -> str:
+    return fname.split("_")[1]
+
+def get_key_IDs() -> "list[str]":
+    system(f'gpg -k > {all_keys_path}')
+    with all_keys_path.open("r") as f:
+        lines = f.readlines()
+        lines_IDs = filter(lambda s: s.startswith(" "), lines)
+        lines_IDs = [line.strip() for line in lines_IDs]
+        key_IDs = filter(lambda s: s != "", lines_IDs)
+        key_files = filter(lambda s: s.startswith("uid"), lines)
+        key_files = [key_file.split("<")[1].split(">")[0] for key_file in key_files]
+    return list(key_IDs)
+
+#### TODO: Get password from key file, add password argument to sign
+
+keys = get_key_IDs()
+
+def sign(fname: str, key_ID: str):
+    system(f"gpg -u {key_ID} -o ./sigs/{fname}.sig --sign ./data/{fname}")
 
 #### TODO: Give the key to clear-sign 
 
@@ -23,4 +44,6 @@ for datum in data:
     for key in keys:
         timeit(lambda: sign(datum, key), number = 1)
 
-#### TODO: sign in right directory; add, commit, push
+#### TODO: sign/verify in right directory; GPG DSA key sizes
+
+
---
+++ run.py
@@ -0,0 +1,6 @@
+import gen_random_data
+import gpg_keys_batch_gen
+import gpg_sign_files
+
+#### TODO run the gen random data; then gpg keys batch; then gpg sign files code
+
