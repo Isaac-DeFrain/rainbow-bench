c9d12dbe55dec3aa8eaa5fe1d38cde59c9d7e3a9
Quantifier <isaacdefrain@gmail.com>
2022-12-03 04:12:21 -0500

    Add verify benchmark stats

--- gpg_verify_sig.py
+++ gpg_verify_sig.py
@@ -1,51 +1,45 @@
-from pathlib import Path
+'''
+Benchmark GPG verify signatures
+'''
+
+from benchmark import *
+from key_ops import *
+from constants import *
 from os import listdir, system
-from constants import KEYS_DIR, SIGS_DIR
-from timeit import timeit
-from gpg_sign_files import key_ids_and_paths, get_key_type
-
-
-sig_types = listdir(SIGS_DIR)
-verify_path = SIGS_DIR / "verify"
+from file_ops import write_file
+from gpg_sign_files import get_key_ids_and_paths
 
 def get_key_len(fname: str) -> str:
     return fname.split("_")[1]
 
 def is_sig_dir(fname: str) -> bool:
     fpath = SIGS_DIR / fname
-    return fpath.is_dir()
+    return fname in KEYS.keys() and fpath.is_dir()
+
+def is_sig_file(fname: str) -> bool:
+    suffix = fname.split(".")[-1]
+    return suffix == "sig"
+
+verify_path = SIGS_DIR / "verify"
 
 #### TODO: Do append correctly
 
 def verify(fname: str, key_id: str, sig_type: str):
+    '''
+    GPG verify signature and append output to `verify_path`
+    '''
     system(f"gpg --batch --yes -u {key_id} --verify {SIGS_DIR / sig_type / fname} >> {verify_path}")
 
-if not verify_path.exists():
-    system(f"touch {verify_path}")
-
-times = {}
-
-# TODO collect and write stats
-
-for sig_type in filter(is_sig_dir, sig_types):
-    sig_dir = SIGS_DIR / sig_type
-    for sig in listdir(sig_dir):
-        key_name = sig.split("-")[0]
-        for key_id, key_path in key_ids_and_paths:
-            if key_name == key_path.name:
-                timeit(lambda: verify(sig, key_id, sig_type), number = 1)
-
-    # groups = map(lambda n: (n, list(filter(lambda fname: fname.split("_")[1] == str(n), keyfiles))), keys[dir_name])
-    # for n, key_files in groups:
-    #     for key_file in key_files:
-    #         try: 
-    #             times[n]
-    #         except KeyError:
-    #             times[n] = []
-    #         times[n].append(timeit(lambda: key_gen(path / key_file), number=1))
-    # fpath = KEYS_DIR / f"{key_type}_stats.json"
-    # if not fpath.exists():
-    #     system(f"touch {fpath}")
-    # with fpath.open("w") as f:
-    #     f.write(dumps(times, indent=4))
-    #     f.close()
+if __name__ == "__main__":
+    times = {}
+    write_file(verify_path)
+    for sig_type in filter(is_sig_dir, listdir(SIGS_DIR)):
+        sig_type_dir = SIGS_DIR / sig_type
+        for sig_name in filter(is_sig_file, listdir(sig_type_dir)):
+            # sig_name = {key_name}-{datum}.sig
+            key_name = sig_name.split("-")[0]
+            n = get_key_len(key_name)
+            for key_id, key_path in get_key_ids_and_paths():
+                if key_name == key_path.name:
+                    benchmark(verify(sig_name, key_id, sig_type), times, n)
+        write_file(SIGS_DIR / f"{sig_type}_verify_stats.json", dumps(times, indent=4))
