d3111b8d1bc2eb387b572b5d43c3ed5520dea6b1
floresclaudiaa <flores.claudia.a@gmail.com>
2022-12-03 17:57:26 +0000

    Add dirs for reference and optimized rainbow

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO
+
+-[ ] Add rainbow and benchmark
+-[ ] Key Sizes for other GPG key types 
+-[ ] readme
+-[ ] Data analysis
--- rainbow.py
+++ rainbow.py
@@ -1,7 +1,19 @@
+#### TODO
 #### Build rainbow automations
 #### Benchmark rainbow reference and optimized and possibly alternative
+#### Decide on variants in Makefile
 
 from os import system
+from pathlib import Path
 
 system("git clone https://github.com/fast-crypto-lab/rainbow-submission-round2.git")
 
+RAINBOW_DIR = Path.cwd() / "rainbow-submission-round2"
+
+#### Reference Implementation
+
+RAINBOW_REF_DIR = RAINBOW_DIR / "Reference_Implementation"
+
+#### Optimized Implementation
+
+RAINBOW_OPT_DIR = RAINBOW_DIR / "Optimized_Implementation/amd64"
