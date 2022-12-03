a97270619cfad269add6cb5712987a9709d7d0eb
floresclaudiaa <flores.claudia.a@gmail.com>
2022-12-03 17:36:52 +0000

    Add git clone rainbow repo

---
+++ rainbow.py
@@ -0,0 +1,7 @@
+#### Build rainbow automations
+#### Benchmark rainbow reference and optimized and possibly alternative
+
+from os import system
+
+system("git clone https://github.com/fast-crypto-lab/rainbow-submission-round2.git")
+
