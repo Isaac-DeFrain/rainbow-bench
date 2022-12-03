30de025d1b75e8a598a057ff122a5763b77378a9
floresclaudiaa <flores.claudia.a@gmail.com>
2022-12-03 17:13:00 +0000

    Fix mk_path in file_ops

--- file_ops.py
+++ file_ops.py
@@ -3,17 +3,18 @@ from constants import *
 from pathlib import Path
 
 def mk_path(fpath: Path):
-    if fpath != Path.cwd():
-        if not fpath.parent.exists():
-            system(f"mkdir {fpath.parent}")
-            mk_path(fpath.parent)
+    parents = fpath.parents
+    idx = parents.index(Path.cwd())
+    for i in range(1, idx + 1):
+        if not parents[idx - i].exists():
+            system(f"mkdir {parents[idx - i]}")
 
 def write_file(fpath: Path, contents: str = ""):
     '''
     Creates dir path from `cwd` to `fpath.parent` and writes `contents` to `fpath.name`
     '''
+    mk_path(fpath)
     if not fpath.exists():
-        mk_path(fpath)
         system(f"touch {fpath}")
     with fpath.open("w", encoding="utf-8") as f:
         f.write(contents)
